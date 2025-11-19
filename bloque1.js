0da6f94a8f5de842f9de4c9820d6c97231bb50d6
eordonezb3-pixel <eordonezb3@unemi.edu.ec>
2025-11-19 14:17:07 -0500

    Update bloque1.js

--- bloque1.js
+++ bloque1.js
@@ -1,2 +1,42 @@
-si leen esto?
-	
+function contarYSumar(arr) {
+    let cantidad = 0;
+    let suma = 0;   
+    arr.forEach(num => {
+        if (num > 3 && num % 5 === 0) {
+            cantidad++;
+            suma += num;
+        }       
+    });
+    console.log("Cantidad =", cantidad);
+    console.log("Suma =", suma);
+}
+
+
+
+
+function validarCedulas(cedulas) {
+    cedulas.forEach(cedula => {
+        let suma = 0;
+        for (let i = 0; i < 9; i++) {
+            let digito = parseInt(cedula.charAt(i));    
+            if (i % 2 === 0) { // índice par
+                digito *= 2;
+                if (digito > 9) {
+                    digito -= 9;
+                }   
+            } // índice impar no necesita cambio
+            suma += digito;
+        }   
+        let residuo = suma % 10;
+        let verificador = (residuo === 0) ? 0 : 10 - residuo;    
+        let ultimoDigito = parseInt(cedula.charAt(9));
+        if (verificador === ultimoDigito) {
+            console.log(`${cedula} → válido`);
+        }   else {
+            console.log(`${cedula} → inválido`);
+        }
+    });
+}
+let cedulas = ["1104680135", "0926687855", "1234567890"];
+validarCedulas(cedulas);    
+
